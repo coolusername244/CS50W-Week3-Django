c36b9d493e732424506567add92109d938283b34
“Lee <leesheppard2404@gmail.com>
2022-10-24 20:09:05 +0200

    initial commit

---
+++ hello/views.py
@@ -0,0 +1,21 @@
+from http.client import HTTPResponse
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "hello/index.html")
+
+def lee(request):
+    return HttpResponse("Hello, Lee!")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
